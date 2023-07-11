1fa5b85580f6e5201e76ba3331c386731418a064
CarlosCamposs <carlosdanct7@gmail.com>
2023-07-10 23:31:07 -0600

    Script

--- scripts/Plot_GitHub.R
+++ scripts/Plot_GitHub.R
@@ -1,4 +1,3 @@
-
 library(readxl)
 
 datos <- read_excel("ReservaMin.xlsx")
@@ -7,12 +6,51 @@ RMatematica <- datos$`Reserva Matemática`
 RMinima <- datos$`Reserva Mínima`
 Tiempo <- datos$Tiempo
 
+# Grafico en README de GitHub
+# ------------------------------------------------------------------------------
+
+##### Puntos y rayas
 
 plot(Tiempo,RMinima,main='Reservas',xlab='Tiempo',ylab='Reserva',
-     col="dodgerblue3",bg="dodgerblue3",pch=21, type='b')
-#ylim(0,200000)
+     col="dodgerblue3",bg="dodgerblue3",pch=21, type='b',
+     ylim = c(0,200000))#b
+#abline(h=c(0,25000,50000,75000,100000,125000,150000,175000,200000),col="gray")
+#lines(x=Tiempo,y=RMinima,col="dodgerblue3",bg="dodgerblue3",pch=21, type='h',lwd=3)
 lines(x=Tiempo,y=RMatematica,col="darkorange1",bg="darkorange1",pch=21, type='b')
-text(20,100000,labels='Igualdad')
-points(20,datos$`Reserva Matemática`[20],col='red',bg='red',pch=21) #
-abline(v=20,col="red")
-legend('topright', legend=c('Reserva Matemática', 'Reserva Mínima'),col=c("darkorange1","dodgerblue3"))
+#lines(x=Tiempo,y=RMatematica,col="darkorange1",bg="darkorange1",pch=21, type='h',lwd=3)
+par(bg = "#f7f7f7")
+lines(x=Tiempo,y=RMatematica,col="darkorange1",bg="darkorange1",pch=21, type='b')
+legend('topleft',col=c('darkorange1','dodgerblue3'), legend=c('Reserva Matemática', 'Reserva Mínima'),
+       lty=2,bty = "n")
+text(20,RMatematica[20]+10000,labels=expression(V[30]^{min}==V[30]),col='red')
+points(20,datos$`Reserva Matemática`[20],col='red',bg='red',pch=21,cex=1.5)
+
+# ------------------------------------------------------------------------------
+
+##### Otra visualizacion pero como si fuera histograma
+
+plot(Tiempo,RMinima,main='Reservas',xlab='Tiempo',ylab='Reserva',
+     col="dodgerblue3",bg="dodgerblue3",pch=21, type='s',
+     ylim = c(0,200000))#b
+lines(x=Tiempo,y=RMatematica,col="darkorange1",bg="darkorange1",pch=21, type='s')
+legend('topleft',col=c('darkorange1','dodgerblue3'), legend=c('Reserva Matemática', 'Reserva Mínima'),
+       lty=2,bty = "n")#
+text(20,RMatematica[20]+10000,labels=expression(V[30]^{min}==V[30]),col='red')
+points(20,datos$`Reserva Matemática`[20],col='red',bg='red',pch=21,cex=1.5) #
+
+# ------------------------------------------------------------------------------
+
+##### Esta es la que aparece en GITHUB
+par(bg = 'gray100')
+plot(Tiempo,RMatematica,main='Reservas',xlab='Tiempo',ylab='Reserva',
+     col="darkorange1",bg="darkorange1",pch=21, type='h',lwd=10,
+     ylim = c(0,200000))#b
+lines(x=Tiempo,y=RMinima,col="skyblue1",bg="skyblue1",pch=21, type='h',lwd=8)
+legend('topleft',col=c('darkorange1','skyblue1'), legend=c('Reserva Matemática', 'Reserva Mínima'),
+       lty=1,bty = "n")#
+text(20,RMatematica[20]+10000,labels=expression(V[30]^{min}==V[30]),col='red')
+points(20,datos$`Reserva Matemática`[20],col='red',bg='red',pch=21,cex=1.5) #
+
+# ------------------------------------------------------------------------------
+
+
